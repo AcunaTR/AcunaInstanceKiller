f131d8b60154924d2c59a21a47f0836fc87cdd9c
Pressman <Annette.Pressman@thomsonreuters.com>
2017-12-19 10:36:48 +0000

    second commit

---
+++ src/main/java/com/thomsonreuters/aws/ec2/wrappers/Ec2EnvImpl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thomsonreuters.aws.ec2.wrappers;
+
+/**
+ *
+ * @author U6067157
+ */
+public class Ec2EnvImpl {
+    
+}

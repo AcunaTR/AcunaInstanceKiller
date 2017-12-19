80aa42692db2996ecf3888c47929b7164db7720f
Dean <Andrew.Dean@thomsonreuters.com>
2017-12-19 11:32:58 +0000

    Added dummy test

---
+++ AcunaCode/src/test/java/com/amazonaws/lambda/demo/BlankTest.java
@@ -0,0 +1,15 @@
+package com.amazonaws.lambda.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BlankTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+
+}

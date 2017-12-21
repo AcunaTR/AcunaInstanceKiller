35dc324080e6777df4b962f7e7d401a849b5f593
Pressman <Annette.Pressman@thomsonreuters.com>
2017-12-21 11:25:57 +0000

    Added ifnd amis example in handler

--- AcunaCode/src/main/java/com/thomsonreuters/lambda/demo/LambdaFunctionHandler.java
+++ AcunaCode/src/main/java/com/thomsonreuters/lambda/demo/LambdaFunctionHandler.java
@@ -1,7 +1,17 @@
 package com.thomsonreuters.lambda.demo;
 
+import java.util.Arrays;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.thomsonreuters.aws.ami.IAmis;
+import com.thomsonreuters.aws.environment.ec2.EC2Env;
+import com.thomsonreuters.aws.environment.ec2.IEC2Env;
+import com.thomsonreuters.aws.filter.Filter;
+import com.thomsonreuters.aws.filter.Filters;
+import com.thomsonreuters.aws.filter.IFilter;
+import com.thomsonreuters.aws.environment.ec2.request.ami.DescribeAmisRequest;
+import com.thomsonreuters.aws.environment.ec2.request.ami.IDescribeAmisRequest;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 
@@ -10,7 +20,15 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
         context.getLogger().log("Input: " + input);
         //test
         // TODO: implement your handler
-        return "Hello again from Lambda!";
+        
+        IEC2Env ec2Env = EC2Env.create();
+        IDescribeAmisRequest req = DescribeAmisRequest.create();
+        IFilter filter = Filter.create("tag:tr-latest", "True");
+        IFilter filter2 = Filter.create("tag:tr-supported", "True");
+        req.setFilters(Filters.create(Arrays.asList(filter, filter2)));
+        IAmis amis = ec2Env.describeAmis(req);
+        
+        return amis.toString();
         
 
     }    

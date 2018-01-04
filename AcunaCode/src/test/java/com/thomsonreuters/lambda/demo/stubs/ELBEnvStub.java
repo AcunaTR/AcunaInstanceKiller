5a1ef9dca0cd5e49b4ec70fbf0830b6122f8431c
Pressman <Annette.Pressman@thomsonreuters.com>
2018-01-04 10:13:24 +0000

    failed test to experiment with rss feed 2

--- AcunaCode/src/test/java/com/thomsonreuters/lambda/demo/stubs/ELBEnvStub.java
+++ AcunaCode/src/test/java/com/thomsonreuters/lambda/demo/stubs/ELBEnvStub.java
@@ -1,21 +1,27 @@
 package com.thomsonreuters.lambda.demo.stubs;
 
+import java.util.List;
+
 import com.amazonaws.services.elasticloadbalancingv2.model.DescribeTargetHealthResult;
 import com.thomsonreuters.aws.environment.elb.IELBEnv;
 import com.thomsonreuters.aws.environment.elb.request.IDeregisterTargetsRequest;
 import com.thomsonreuters.aws.environment.elb.request.IDescribeTargetGroupsRequest;
 import com.thomsonreuters.aws.environment.elb.request.IDescribeTargetHealthRequest;
+import com.thomsonreuters.aws.targetgroup.ITargetGroup;
 import com.thomsonreuters.aws.targetgroup.ITargetGroups;
 
 public class ELBEnvStub implements IELBEnv {
 
 	private IDescribeTargetGroupsRequest _req;
+	private List<ITargetGroup> _targetGroup;
 	
 	@Override
 	public ITargetGroups describeTargetGroups(IDescribeTargetGroupsRequest req) {
-		_req = req;
-		
-		return null;
+
+		ITargetGroup targetGroup = null;
+		_targetGroup.add(targetGroup);
+		ITargetGroups targetGroups = (ITargetGroups) _targetGroup;
+		return targetGroups;
 	}
 
 	@Override
